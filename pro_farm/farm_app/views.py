cc09ec764bdab5cad374fbac9bea6f814664b83a
Ayush Malik <ayushmalik779@gmail.com>
2020-08-24 23:26:29 +0530

    Added context dict to pass variables to html page

--- pro_farm/farm_app/views.py
+++ pro_farm/farm_app/views.py
@@ -1,7 +1,20 @@
+# from django.shortcuts import render
+
+# # Create your views here.
+
+# def home_page(request):
+#     context = {}
+#     return render(request , 'home.html' , context)
+
+
 from django.shortcuts import render
+from modules.imp_funcs import *
 
 # Create your views here.
 
 def home_page(request):
-    context = {}
+    context = {
+        'var_1' : translator("Hello") ,
+        'var_2' : OTP_generator()
+        }
     return render(request , 'home.html' , context)
