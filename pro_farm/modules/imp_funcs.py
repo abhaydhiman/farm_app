b2c45d91bee9ba478ef8838f902b2f95c28e6986
Ayush Malik <ayushmalik779@gmail.com>
2020-08-26 11:12:00 +0530

    Now translator doesn't translate to english language

--- pro_farm/modules/imp_funcs.py
+++ pro_farm/modules/imp_funcs.py
@@ -1,11 +1,14 @@
 from translate import Translator
 import math, random
 #________________________________________________________________________________________________________________________
-
-def translator(text , to_langg = 'hindi'):
-    translator= Translator(to_lang = to_langg)
-    translated_text = translator.translate(text)
-    return translated_text
+# This function translates english to any other language
+def translator(text , to_langg = 'hindi'): 
+    if to_langg.lower() != 'english':
+        translator= Translator(to_lang = to_langg)
+        translated_text = translator.translate(text)
+        return translated_text
+    else:
+        return text
 #_______________________________________________________________________________________________________________________
 def OTP_generator():
     # for alpha nuemeric OTP 
@@ -13,7 +16,7 @@ def OTP_generator():
     generate_OTP = "" 
     size = 5 # length of generated OTP
     length = len(corpus) 
-    for i in range(size) : 
+    for i in range(size) :  
         generate_OTP += corpus[math.floor(random.random() * length)] 
     return generate_OTP
 #_______________________________________________________________________________________________________________________
