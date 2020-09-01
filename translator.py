4f477af41ccdaefbb32e16c0d6aec632714f4751
Aaditya1978 <aadityasinghal1978@gmail.com>
2020-09-01 18:08:18 +0530

    added function for translating user_home page

--- translator.py
+++ translator.py
@@ -1,6 +1,6 @@
 # Importing Required Libraries
 from googletrans import Translator
-
+from pages_text import user_home_text
 
 # Init For GoogleTrans
 trans = Translator()
@@ -33,3 +33,15 @@ def text_translator(text_ls, lang):
         translated = trans.translate(text, src='en', dest=lang)
         translated_ls.append(translated.text)
     return translated_ls
+
+
+
+# Function for translating User Home Text to the language selected by user
+def user_home_text_translator(lang, name):
+    text_ls = user_home_text()
+    translated_ls = []
+    for text in text_ls:
+        translated = trans.translate(text, src='en', dest=lang)
+        translated_ls.append(translated.text)
+    name = trans.translate(name, src='en', dest=lang)
+    return translated_ls, name.text
