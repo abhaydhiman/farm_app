60c46fe8e237f4111237f1d4f09e5af9792464c3
Aaditya1978 <aadityasinghal1978@gmail.com>
2020-09-05 13:37:43 +0530

    added function for translating wether page text and also to covert given text into english

--- translator.py
+++ translator.py
@@ -1,6 +1,6 @@
 # Importing Required Libraries
 from googletrans import Translator
-from pages_text import user_home_text
+from pages_text import user_home_text, weather_text
 
 # Init For GoogleTrans
 trans = Translator()
@@ -26,6 +26,8 @@ def language_checker(language):
 
     return lang
 
+
+
 # Function for translating text to a given language
 def text_translator(text_ls, lang):
     translated_ls = []
@@ -37,11 +39,29 @@ def text_translator(text_ls, lang):
 
 
 # Function for translating User Home Text to the language selected by user
-def user_home_text_translator(lang, name):
+def user_home_text_translator(lang, name, city, state):
     text_ls = user_home_text()
     translated_ls = []
     for text in text_ls:
         translated = trans.translate(text, src='en', dest=lang)
         translated_ls.append(translated.text)
     name = trans.translate(name, src='en', dest=lang)
-    return translated_ls, name.text
+    city = trans.translate(city, src='en', dest=lang)
+    state = trans.translate(state, src='en', dest=lang)
+    return translated_ls, name.text, city.text, state.text
+
+
+# Function for translating Weather page text to the language selected by user
+def weather_text_translator(lang, city, state):
+    text_ls = weather_text()
+    translated_ls = []
+    for text in text_ls:
+        translated = trans.translate(text, src='en', dest=lang)
+        translated_ls.append(translated.text)
+    city = trans.translate(city, src='en', dest=lang)
+    state = trans.translate(state, src='en', dest=lang)
+    return translated_ls, city.text, state.text
+
+def english_translator(text):
+    translated = trans.translate(text, dest='en')
+    return translated.text
