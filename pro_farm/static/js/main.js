c41d141451d58771141da997ae1e84aedc8bec45
Abhay_Dhiman <abhaysdhimans@gmail.com>
2020-09-16 10:23:08 +0530

    Added styling and responsiveness for shop details page

--- pro_farm/static/js/main.js
+++ pro_farm/static/js/main.js
@@ -160,6 +160,28 @@
     });
 
 
+    /*-------------------
+        Quantity change
+    --------------------- */
+    var proQty = $('.pro-qty');
+    proQty.prepend('<span class="dec qtybtn">-</span>');
+    proQty.append('<span class="inc qtybtn">+</span>');
+    proQty.on('click', '.qtybtn', function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        $button.parent().find('input').val(newVal);
+    });
+
     /*-----------------------------
         Product Discount Slider
     -------------------------------*/
