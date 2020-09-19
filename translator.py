6f14c2451cd2376920529e8a37efe21116444512
Aaditya1978 <aadityasinghal1978@gmail.com>
2020-09-19 16:21:34 +0530

    added functions for translating text for insurance form

--- translator.py
+++ translator.py
@@ -1,12 +1,17 @@
-# Importing Required Libraries
+# Importing Google Translator Python Library
 from googletrans import Translator
+
+# Importing Functions From pages_text.py File
 from pages_text import user_home_text, weather_text, user_home_text2, user_home_text3, user_home_text4,\
-    user_home_text5, financial_text,loan_text,loan_text2,loan_text3,loan_text4,loan_text5
+user_home_text5, financial_text,loan_text,loan_text2,loan_text3,loan_text4,loan_text5,insu_text,insu_text2,\
+insu_text3,insu_text4,insu_text5
+
 
 # Init For GoogleTrans
 trans = Translator()
 
 
+#_____________________________________________________________________
 # Fuction for checking the language selected by user
 def language_checker(language):
 
@@ -28,7 +33,7 @@ def language_checker(language):
     return lang
 
 
-
+#_____________________________________________________________________
 # Function for translating text to a given language
 def text_translator(text_ls, lang):
     translated_ls = []
@@ -39,6 +44,7 @@ def text_translator(text_ls, lang):
 
 
 
+#___________________________________________________________________________
 # Function for translating User Home Text to the language selected by user
 def user_home_text_translator(lang, name, city, state):
     translated_ls = []
@@ -60,6 +66,8 @@ def user_home_text_translator(lang, name, city, state):
     return translated_ls, name.text, city.text, state.text
 
 
+
+#_____________________________________________________________________________
 # Function for translating Weather page text to the language selected by user
 def weather_text_translator(lang, city, state):
     text_ls = weather_text()
@@ -73,12 +81,16 @@ def weather_text_translator(lang, city, state):
 
 
 
+#_____________________________________________________________________
+# Function for translating given text of any language to english
 def english_translator(text):
     translated = trans.translate(text, dest='en')
     return translated.text
 
 
 
+#_____________________________________________________________________
+# Function for translating Financial Page Text
 def financial_translator(lang):
     text_ls = financial_text()
     translated_ls = []
@@ -89,7 +101,8 @@ def financial_translator(lang):
 
 
 
-# Function for translating User Home Text to the language selected by user
+#____________________________________________________________________________
+# Function for translating Loan Form Text to the language selected by user
 def loan_text_translator(lang, t1, t2, t3):
     translated_ls = []
     if lang == 'en':
@@ -104,6 +117,29 @@ def loan_text_translator(lang, t1, t2, t3):
         translated_ls = loan_text5()
     elif lang == 'hi':
         translated_ls = loan_text2()
+    t1 = trans.translate(t1, src='en', dest=lang)
+    t2 = trans.translate(t2, src='en', dest=lang)
+    t3 = trans.translate(t3, src='en', dest=lang)
+    return translated_ls, t1.text, t2.text, t3.text
+
+
+
+#__________________________________________________________________________
+# Function for translating Insurance Form Text to the language selected by user
+def insu_text_translator(lang, t1, t2, t3):
+    translated_ls = []
+    if lang == 'en':
+        translated_ls = insu_text()
+    elif lang == 'hi':
+        translated_ls = insu_text2()
+    elif lang == 'pa':
+        translated_ls = insu_text3()
+    elif lang == 'mr':
+        translated_ls = insu_text4()
+    elif lang == 'ta':
+        translated_ls = insu_text5()
+    elif lang == 'hi':
+        translated_ls = insu_text2()
     t1 = trans.translate(t1, src='en', dest=lang)
     t2 = trans.translate(t2, src='en', dest=lang)
     t3 = trans.translate(t3, src='en', dest=lang)
