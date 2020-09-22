c6322bc3dfb32db19ef95e22a92dd8be3d8d611f
Ayush Malik <ayushmalik779@gmail.com>
2020-09-22 15:36:10 +0530

    Modified the urls of auth app

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -13,24 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path , include
 from auth_app import views
+import shop as shop
 
 urlpatterns = [
-    path('' , views.lang_select , name = 'lang_select_page'),
-
-    path('signup' , views.signup_page , name = 'signup_page'),
-
-    # path('submit_signup_data' , views.submit_signup_data , name = 'submit_signup_data_page'),
-
-    path('user_home' , views.user_home , name = 'user_home_page'),
-
-    path('login_page' , views.login_page , name = 'login_page') ,
-
-    path('check_login_details' , views.check_login_details , name = 'check_login_details') ,
-
+    path(''                      , views.lang_select           , name = 'lang_select_page'),
+    path('signup'                , views.signup_page           , name = 'signup_page'),
+    path('login_page'            , views.login_page            , name = 'login_page') ,
+    path('check_login_details'   , views.check_login_details   , name = 'check_login_details') ,
     path('submit_signup_details' , views.submit_signup_details , name = 'submit_signup_details') ,
-
     path('verify_signup_details' , views.verify_signup_details , name = 'verify_signup_details') ,
+    path('check_password'        , views.check_password        , name = 'check_password_details') ,
+
+
+    # ? Including the URLS of apps(user_home and shop_home)
+    path('user_home/' , include('user_home.urls')) , # for using urls of user_home app [very very important] 
+    path('shop_home/' , include('shop.urls')) ,     # for using urls of shop app [very very important]
+
+    # path('shop_home/add_item_to_cart' , shop.views.product_details),
+
     
+
 ]
