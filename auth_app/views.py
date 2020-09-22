71c5ff1a438bc05519a98cc6f0cc5f58105b0536
Ayush Malik <ayushmalik779@gmail.com>
2020-09-22 15:36:30 +0530

    Added more views

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,15 +1,69 @@
-from django.shortcuts import render
-from modules.imp_funcs import *
+from django.shortcuts import render , redirect
 
 
+# from modules.imp_funcs import *
 
 # ***********************************************************************
-# import pyrebase
+
+
+'''
+All the given function are imported from { modules/imp_func.py }
+'''
+# from modules.imp_funcs  import  translator       # Translator translates the context texts
+from modules.imp_funcs  import  OTP_generator      # This function is used to generate OTP
+from modules.imp_funcs  import  msg_sender         # This function is used to send OTP for signup 
+from modules.imp_funcs  import  context_generator  # All functions of auth_app uses this function to generate context
+from firebase_manager   import  FirebaseManager    # Importing the FirebaseManager class 
+from firebase_manager   import  User               # Importing the User class
+
+# ***********************************************************************
+
+
+'''
+NOTE -> Importing firebaseConfig dictationary from { modules/imp_func.py }
+'''
+from modules.imp_funcs import firebaseConfig
 from pyrebase import pyrebase
+
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 db = firebase.database()
+
+
 # ***********************************************************************
+
+
+
+
+
+
+
+
+# *?-----------------------------------FLOW OF VIEW FUNCTIONS--start------------------------------|
+'''
+NOTE -> All the functions that are mentioned below are defined in in order of flow
+
+
+# * lang_select --->  shifts control to {signup_page}
+
+# * signup_page --->  it has two buttons , **Only allows unregistered numbers for signup**
+                                        >> 1 Generate OTP button for [signup]             --> shifts control to {submit_signup_details}
+                                        >> 2 Already a user , Log in button for [login]   --> shifts control to {Login_page}
+
+# * submit_signup_details  
+
+# * verify_signup_details
+
+# * login_page
+
+# * check_login_details
+
+# * user_home
+
+
+'''
+# *?-----------------------------------FLOW OF VIEW FUNCTIONS--end------------------------------|
+
 
 
 
@@ -19,9 +73,13 @@ db = firebase.database()
 
 
 def lang_select(request):
+    # request.session.flush()
+    session_keys = list(request.session.keys())
+    for key in session_keys:
+        del request.session[key] # Deleting all previous session variables
 
     context = {}
-    return render(request , 'lang_select.html' , context)
+    return render(request , 'auth_app/lang_select.html' , context)
 
 
 # *------------------------------------------------------------------------------------------------------------------*
@@ -31,80 +89,104 @@ def signup_page(request):
 
     if request.method == 'POST': 
 
-        request.session['language_selected']  = request.POST['language_selected'] 
+        user                    = User(request.POST['language_selected']) # Creating a User class object
+        request.session['user'] = [user]                                    # Storing the user object as a session variabels
 
-        language_selected = request.session['language_selected']
+        context = context_generator(for_page = 'signup_page' ,
+                                    language_selected =  user.language ,
+                                    texts_to_be_added = None)
 
-        context = context_generator(for_page = 'signup_page' ,language_selected =  language_selected , texts_to_be_added = None)
-
-        return render(request , 'signup.html' , context)
+        return render(request , 'auth_app/signup.html' , context)
 
 
 # *------------------------------------------------------------------------------------------------------------------*
 
 
-def submit_signup_details(request):
+# **USE OF IF-ELSE here --> Here user is allowed to generate an OTP if its number is not registered in our firebase** else user recieves an error message
 
-    if request.method == 'POST' and ( str(request.POST['Phone_number']) not in db.child('General_User').get().val() ) :
+def submit_signup_details(request):
+    # user              = request.session['user']        # Getting the user object which is a session variable here
+    # user.phone_number = request.POST['Phone_number']   # Adding the phone_number of user to user object 
+
+
+    if request.method == 'POST' and not user.isRegistered():
 
         language_selected                       = request.session['language_selected']
-
         request.session['phone_number_of_user'] = request.POST['Phone_number']
         request.session['name_of_user']         = request.POST['name']
 
-        
+        context = context_generator(for_page = 'submit_signup_details'  , 
+                                    language_selected = language_selected , 
+                                    texts_to_be_added = [['generated_otp' , OTP_generator()]] )
 
-        context = context_generator(for_page = 'submit_signup_details'  , language_selected = language_selected , texts_to_be_added = [['generated_otp' , OTP_generator()]] )
+        # msg_sender( given_phone_number = request.POST['Phone_number'] , given_message = str(context['generated_otp']) )
 
-        print(context['generated_otp'])
-
-        msg_sender( given_phone_number = request.POST['Phone_number'] , given_message = str(context['generated_otp']) )
-
-        return render(request , 'signup.html' , context)
+        return render(request , 'auth_app/signup.html' , context)
     else:
         language_selected       = request.session['language_selected']
-
-        context = context_generator(for_page = 'signup_page' ,language_selected =   language_selected ,
+        context = context_generator(for_page = 'signup_page' ,
+                                    language_selected =   language_selected ,
                                     texts_to_be_added = [['warning_msg' ,"Phone Number already registered"]])
         
-        return render(request , 'signup.html' , context)
+        return render(request , 'auth_app/signup.html' , context)
 
 
 # *------------------------------------------------------------------------------------------------------------------*
 
+
+# **USE OF IF-ELSE here --> Here user is only registered in our firebase if OTP  by user is correct**
 
 def verify_signup_details(request):
     context = {}
 
     if request.POST['generated_otp'] == request.POST['otp_given_by_user']:
-
         phone_number_of_user = request.session['phone_number_of_user']
         name_of_user         = request.session['name_of_user']
         language_selected    = request.session['language_selected']
 
+        context = context_generator(for_page = 'set_password' , language_selected = language_selected)
+        return render(request , 'auth_app/set_password.html' , context)
+
+    else:
+        language_selected       = request.session['language_selected']
+        context = context_generator(for_page = 'submit_signup_details' ,
+                                    language_selected =   language_selected ,
+                                    texts_to_be_added =  [['warning_msg' ,"OTP entered is incorrect"] , 
+                                    ['generated_otp' , request.POST['generated_otp']] ])
+        return render(request , 'auth_app/signup.html' , context)
+
+
+# *------------------------------------------------------------------------------------------------------------------*
+# Firebase me password 6 digit ka hona hi chahiye
+# ?dono password equal na ho to error msg else statement me 
+def check_password(request):
+
+    phone_number_of_user = request.session['phone_number_of_user']
+    name_of_user         = request.session['name_of_user']
+    language_selected    = request.session['language_selected']
+
+
+    if request.POST['password_1'] == request.POST['password_2'] :
         email = str(phone_number_of_user) + "@farmapp.com"
-        password = request.POST['otp_given_by_user']
-
-        user = auth.create_user_with_email_and_password(email , password)
-
-        data = {'Phone Number' : phone_number_of_user ,
+        user = auth.create_user_with_email_and_password(email , request.POST['password_1'])
+        data = {'phone_number' : phone_number_of_user ,
                 'language'     : language_selected ,
                 'name'         : name_of_user , 
+                'state'        : request.POST['state'] , 
+                'city'         : request.POST['city'] , 
+                'cart'         : {'isEmpty' : True } , 
         }
 
         db.child('General_User').child(str(phone_number_of_user)).set(data)
+        context = {}
 
-
-
-        return render(request , 'user_home.html' , context)
+        request.session['loggedin_user_phone_number'] = phone_number_of_user
+        return redirect('user_home:home_page')
     else:
-        language_selected       = request.session['language_selected']
-
-        context = context_generator(for_page = 'submit_signup_details' ,language_selected =   language_selected ,
-                                texts_to_be_added =  [['warning_msg' ,"OTP entered is incorrect"] , ['generated_otp' , request.POST['generated_otp']] ])
+        context = context_generator(for_page  = 'set_password')
+        context = {}
+        return render(request , 'auth_app/set_password.html' , context)
     
-        return render(request , 'signup.html' , context)
-            
 
 # *------------------------------------------------------------------------------------------------------------------*
 
@@ -112,44 +194,47 @@ def verify_signup_details(request):
 def login_page(request):
     if request.method == 'POST' :
         language_selected  = request.session['language_selected']
-
-        context = context_generator(for_page = 'login_page', language_selected =   language_selected , texts_to_be_added =  None)
-
-        return render(request , 'login_page.html' , context)
+        context = context_generator(for_page = 'login_page', 
+                                    language_selected =   language_selected ,
+                                    texts_to_be_added =  None)
+        return render(request , 'auth_app/login_page.html' , context)
 
 
 # *------------------------------------------------------------------------------------------------------------------*
 
 
-def check_login_details(request):
-    context = {}
-    if request.method == 'POST' :
+# **USE OF TRY-EXCEPT here --> If user enters correct phone number and password , then he is allowed to login**
 
+def check_login_details(request):
+    if request.method == 'POST' :
+        context = {}
         phone_number  = request.POST['Phone_number']
         email         = str(phone_number) + "@farmapp.com"
         password      = request.POST['password']
 
         try:
-
             user = auth.sign_in_with_email_and_password(email , password)
-
-            return render(request , 'user_home.html' , context)
+            request.session['loggedin_user_phone_number'] = phone_number
+            return redirect('user_home:home_page')
 
         except:
-
             language_selected  = request.session['language_selected']
-
-            context = context_generator(for_page = 'check_login_details'  ,language_selected =  language_selected ,
+            context = context_generator(for_page = 'check_login_details'  ,
+                                        language_selected =  language_selected ,
                                         texts_to_be_added = [[' warning_msg' ,"Either phone number or password is incorrect" ]])
-            return render(request , 'login_page.html' , context)
+            return render(request , 'auth_app/login_page.html' , context)
 
 
-## *------------------------------------------------------------------------------------------------------------------*
+# *------------------------------------------------------------------------------------------------------------------*
 
 
-def user_home(request):
+# def user_home(request):
+#     return redirect('user_home:home_page') # for redirecting to urls of user_home app of this project
+    
+    
+# # *------------------------------------------------------------------------------------------------------------------*
+
+
+# def weather(request):
     context = {}
-    return render(request , 'user_home.html' , context)
-
-
-## *------------------------------------------------------------------------------------------------------------------*
+    return render(request , 'auth_app/weather.html' , context)
