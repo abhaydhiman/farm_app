cdf90f1cd56999fe7c67140f039442869012ad14
Aaditya1978 <aadityasinghal1978@gmail.com>
2020-10-14 21:34:31 +0530

    added routes to seller, shop pages and to the disease prediction pages

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from flask import Flask, render_template,request,session, jsonify
 # Importing Functions from modules.py
 from modules import opt_generator, msg_sender, firebase_user_checker, firebase_user_registerer,\
 firebase_user_loger, firebase_data_fetcher, weather_fetcher, day_fetcher, loan_data, get_current_time,\
-loan_data_fetcher, insu_data, insu_data_fetcher, update_status, product_data_sender,product_data_fetcher
+loan_data_fetcher, insu_data, insu_data_fetcher, update_status, product_data_sender,product_data_fetcher,\
+product_data_fetcher2, product_data_updater, product_remover, user_image_sender, user_image_fetcher,\
+all_product_fetcher, buy_product_data, user_cart_data_sender, user_cart_data_fetcher
 
 
 # Importing Functions For translation from translator.py
@@ -22,14 +24,14 @@ app.secret_key = 'any random string'
 
 # Route for home page i.e. Language Selection Page
 @app.route('/')
-def home(): 
+def home():
     return render_template('new_lang_select.html')
 
 
 
 # Route for Sign Up page i.e. Page For Authentication
 @app.route('/sign_up', methods=['POST'])
-def sign_up(): 
+def sign_up():
     if request.method == 'POST':
         language = request.form['submit_button']
 
@@ -116,7 +118,7 @@ def set_password():
 
 
 
-# TO Go To Home Page If Farmer Follows the Step to Register 
+# TO Go To Home Page If Farmer Follows the Step to Register
 @app.route('/user_home', methods=['POST'])
 def user_home():
     if request.method == 'POST':
@@ -139,8 +141,9 @@ def user_home():
             session['state'] = state
             session['city'] = city
             session['status'] = status
-            return render_template('user_home.html', t_lis = translated_list, phone_number = phone_number, name = name, city=city, state = state, status=status)
-        
+            image = user_image_fetcher(phone_number)
+            return render_template('user_home.html', t_lis = translated_list, phone_number = phone_number, name = name, city=city, state = state, status=status, image=image)
+
         # If condition not matched then show error
         else:
             text_ls = ['Submit Password', 'Enter Password', 'Confirm Password', 'Confirm', 'Both Password Do not match / password must be greater then 6 digits']
@@ -151,7 +154,7 @@ def user_home():
 
 
 
-# TO Go To Home Page If Farmer Follows the Step to Login 
+# TO Go To Home Page If Farmer Follows the Step to Login
 @app.route('/user_home_2', methods=['POST'])
 def user_home_2():
     if request.method == 'POST':
@@ -173,15 +176,16 @@ def user_home_2():
                 session['state'] = state
                 session['city'] = city
                 session['status'] = status
-                return render_template('user_home.html', t_lis = translated_list, phone_number = phone_number, name = name,city = city, state = state, status=status)
+                image = user_image_fetcher(phone_number)
+                return render_template('user_home.html', t_lis = translated_list, phone_number = phone_number, name = name,city = city, state = state, status=status,image=image)
 
-            # Otherwise Then error is shown 
+            # Otherwise Then error is shown
             else:
                 text_ls = ['Login', 'Phone Number', 'Password', 'Submit', 'Cancel', 'Password Or Phone Number Did not match']
                 log_list = text_translator(text_ls, lang)
                 return render_template('log_in.html', log_list = log_list, lang=lang, mm=log_list[5])
 
-        # If user wnats to go back to signup Page 
+        # If user wnats to go back to signup Page
         if request.form['log-button'] == 'back':
             text_ls = ['Enter Credentials', 'Phone Number', 'Full Name', 'Submit', 'Already Registerd, Login']
             sign_up_list = text_translator(text_ls, lang)
@@ -190,7 +194,7 @@ def user_home_2():
 
 
 
-# TO Go To Home Page If User Clicks on Home LINK on any Page 
+# TO Go To Home Page If User Clicks on Home LINK on any Page
 @app.route('/user_home_3', methods=['POST','GET'])
 def user_home_3():
     name = session['name']
@@ -199,8 +203,9 @@ def user_home_3():
     state = session['state']
     city = session['city']
     status = session['status']
+    image = user_image_fetcher(phone_number)
     translated_list, name, city, state, status = user_home_text_translator(lang, name, city, state,status)
-    return render_template('user_home.html', t_lis = translated_list, phone_number = phone_number, name = name, city = city, state = state, status=status)
+    return render_template('user_home.html', t_lis = translated_list, phone_number = phone_number, name = name, city = city, state = state, status=status,image=image)
 
 
 
@@ -231,10 +236,21 @@ def profile():
     state = session['state']
     city = session['city']
     tstatus = session['status']
+    image = user_image_fetcher(phone_number)
     date,time, status = loan_data_fetcher(phone_number)
     date2,time2,status2 = insu_data_fetcher(phone_number)
     ls = text_translator([city, state, name, 'Profile','Upload Photo', 'Loan', 'Insurance', tstatus], lang)
-    return render_template('profile.html',loan=ls[5],insu=ls[6],text = ls[3],text2 = ls[4],name = ls[2],phone_number = phone_number,city = ls[0],state = ls[1], date = date, time = time, length=len(date), status = status,date2 = date2, time2 = time2, length2=len(date2), status2 = status2, tstatus=tstatus)
+    return render_template('profile.html',loan=ls[5],insu=ls[6],text = ls[3],text2 = ls[4],name = ls[2],phone_number = phone_number,city = ls[0],state = ls[1], date = date, time = time, length=len(date), status = status,date2 = date2, time2 = time2, length2=len(date2), status2 = status2, tstatus=tstatus, image=image)
+
+
+
+# To Send User Image Data to Database
+@app.route('/user_image', methods=['POST','GET'])
+def user_image():
+    phone_number = session['phone_number']
+    image = request.files.get('image')
+    user_image_sender(phone_number,image)
+    return jsonify({'name':image})
 
 
 
@@ -360,11 +376,12 @@ def shop():
     lang = session['lang']
     phone_number = session['phone_number']
     status = session['status']
+    pname,pdes,price,stock,cat,image, buyers, unique_cat = all_product_fetcher()
     if status == 'Farmer':
         b_text = 'Become Seller'
     else:
         b_text = 'Sell Products'
-    return render_template('shop_now.html', b_text = b_text)
+    return render_template('shop_now.html', b_text = b_text, pname = pname, price=price, image = image, cat = cat, unique_cat = unique_cat, length = len(pname))
 
 
 
@@ -406,10 +423,136 @@ def product():
     des = request.form['des']
     stock = request.form['stock']
     image = request.form['image']
+    cat = cat.replace(' ','-')
     flag = product_data_sender(phone_number=phone_number,name=name,price=price,category=cat,des=des,in_Stock=stock,image=image)
-     
-    return jsonify({'name' : name,'price':price,'cat':cat,'des':des,'stock':stock, 'image':image,'flag':flag})
 
+    html = '<tbody><tr onclick="show_hide_row({});"><td class="shoping__cart__item">\<img id="{}_image" src="{}" alt="">\
+            <h5 id="{}_name">{}</h5></td><td id="{}_cat" class="shoping__cart__price">{}</td>\
+            <td id="{}_price" class="shoping__cart__price">{}</td><td id="{}_stock" class="shoping__cart__price">{}\
+            </td><td id="{}_buy" class="shoping__cart__price">{}</td><td class="shoping__cart__item__close">\
+            <span class="fa fa-close"></span></td></tr>\<tr id="{}" class="shoping__cart__price hidden_row ftco-animated">\
+            <td colspan=4 id="{}_des">{}</td><td class="shoping__cart__item__price"><a href="#hidden_row12" onclick="show_hide_row({});">\
+            <button class="primary-btn cart-btn bedit" value="{}" type="submit" name="wow">Edit</button>\
+            </a></td></tr></tbody>\
+            <tbody id="new_product_row"></tbody>'.format("'" + name + "'",name,image,name,name,name,cat,name,price,name,stock,name,0,name,name,des,"'" + "hidden_row12" + "'",name)
+
+    return jsonify({'flag':flag,'html':html})
+
+
+
+
+# To Send Updated product Data on Seller Page As per Given Name
+@app.route('/pedit',methods=['POST','GET'])
+def pedit():
+    name = request.form['name']
+    phone_number = session['phone_number']
+    price,des,stock,cat,buyers,img = product_data_fetcher2(phone_number,name)
+    return jsonify({'name':name,'price':price,'des':des,'stock':stock,'cat':cat,'buy':buyers,'img':img})
+
+
+
+
+# To Send Updated product Data on Seller Page And to database
+@app.route('/update_product',methods=['POST','GET'])
+def update_product():
+    phone_number = session['phone_number']
+    name = request.form['name']
+    price = request.form['price']
+    cat = request.form['cat']
+    des = request.form['des']
+    stock = request.form['stock']
+    image = request.form['image']
+    product_data_updater(phone_number=phone_number,name=name,price=price,category=cat,des=des,in_Stock=stock,image=image)
+    return jsonify({'name':name,'price':price,'des':des,'stock':stock,'cat':cat,'img':image})
+
+
+
+
+# To delete product Data on Seller Page And from database
+@app.route('/product_remove',methods=['POST','GET'])
+def product_remove():
+    phone_number = session['phone_number']
+    name = request.form['name']
+    product_remover(phone_number, name)
+    return jsonify({'name':name})
+
+
+
+
+# To go to shop_details page to buy the selected product
+@app.route('/buy_product',methods=['POST','GET'])
+def buy_product():
+    phone_number = session['phone_number']
+    pname = request.form['product_buy']
+    price1,des,stock1,cat1,buy,img = buy_product_data(pname)
+    return render_template('shop_details.html', pname = pname, price = price1, des = des, stock = stock1, cat = cat1, buy = buy, img = img)
+
+
+
+
+# To go to sho grid page if user clicks on different categories card.
+@app.route('/product_grid', methods=['POST','GET'])
+def product_grid():
+    phone_number = session['phone_number']
+    product_cat = request.form['product_cat']
+    return render_template('shop_grid.html')
+
+
+
+
+# To go to checkout page or cart page if user clicks on buy now or add to cart button
+@app.route('/buy_now', methods=['POST','GET'])
+def buy_now():
+    phone_number = session['phone_number']
+    pname = request.form['name']
+    amount = request.form['amount']
+    buy2 = request.form['buy']
+    price1,des,stock1,cat1,buy,img = buy_product_data(pname)
+    price1 = int(price1)
+    price1 = price1*int(amount)
+    if price1 > 400:
+        price12= price1 + 20
+    else:
+        price12 = price1 + (price1*(20/100))
+    if buy2 == 'buy':
+        return render_template('checkout.html', pname = pname, price = price1, price12 = price12, des = des, stock = stock1, cat = cat1, buy = buy, img = img, amount = amount)
+    else:
+        user_cart_data_sender(phone_number, pname, amount)
+        name_lis, amount_lis = user_cart_data_fetcher(phone_number)
+        price_lis = []
+        des_lis = []
+        img_lis = []
+        for nam in name_lis:
+            price1,des,stock1,cat1,buy,img = buy_product_data(nam)
+            price_lis.append(price1)
+            img_lis.append(img)
+        price12 = []
+        for i in range(len(price_lis)):
+            prices = int(price_lis[i])*int(amount_lis[i])
+            price12.append(prices)
+        sum_price = sum(price12)
+        totp_price = sum_price + (sum_price*(20/100))
+        return render_template('shoping_cart.html', name_lis = name_lis,price_lis=price_lis,img_lis = img_lis, amount_lis=amount_lis, price12 = price12,length = len(name_lis), sum_price=sum_price, totp_price = totp_price)
+
+
+
+
+# To Go to disease Prediction Page if the user clicks on the disease prediction card.
+@app.route('/disease_prediction', methods=['POST','GET'])
+def disease_prediction():
+    phone_number = session['phone_number']
+    return render_template('disease_prediction.html')
+
+
+
+
+# To Show the Different categories of Disease Cards based on the Main Card Clicked
+@app.route('/disease', methods=['POST','GET'])
+def disease():
+    phone_number = session['phone_number']
+    name = request.form['name']
+    print(name)
+    return jsonify({'name':name})
 
 
 
@@ -418,4 +561,3 @@ def product():
 # Running the Flask App
 if __name__ == "__main__":
     app.run(debug=True)
-
